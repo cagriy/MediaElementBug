d1bf849c45c03cfa2f8cb49ec8004beece7dc9f6
cagri <cyucel@gmail.com>
2023-12-18 17:41:50 +0000

    Initial

---
+++ MediaElement/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MediaElement;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ MediaElement/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MediaElement;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
